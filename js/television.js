8a0a8ccb67a44b71055bc4d785ce322bf2cadc2a
Sebastian Gonzalez Rodriguez <sebgonrod@gmail.com>
2023-07-15 19:17:44 -0500

    NUEVO DISEÑO

--- js/television.js
+++ js/television.js
@@ -1,6 +1,9 @@
 callBack = datos =>{
     document.getElementById("titulo1").innerHTML = datos[0].titulo;
     document.getElementById("titulo2").innerHTML = datos[1].titulo;
+    document.getElementById("titulo3").innerHTML = datos[2].titulo;
+    document.getElementById("titulo4").innerHTML = datos[3].titulo;
+    document.getElementById("titulo5").innerHTML = datos[4].titulo;
     
 }
 
